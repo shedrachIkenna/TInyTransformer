2eac95c101d00983b052bd5f20409f7c5ee8b896
ShedrachIkenna <Shedrach686@gmail.com>
2025-10-24 21:03:01 +0100

    added decode and encode functions

--- tiny_transformer.py
+++ tiny_transformer.py
@@ -17,4 +17,15 @@ vocab_size = len(chars)
 stoi = {ch:i for i,ch in enumerate(chars)}  #This builds a dictionary (mapping) from each character (ch) to a number (i).
 itos = {i:ch for ch,i in stoi.items()} # This does the reverse of stoi: creates a dictionary that maps each number back to a character.
 
+def encode(s):
+    """
+    This function takes in a string as an input and returns its numerical representation based on the stoi mappings 
+    """
+    return [stoi[c] for c in s]
+
+def decode(l): 
+    """
+    Takes in a list of numbers and returns a string based in itos mappings 
+    """
+    return "".join(itos[i] for i in l)
 
