fe71ab081cbe081672cac90a120949862af66434
ShedrachIkenna <Shedrach686@gmail.com>
2025-10-26 22:50:06 +0100

    Added positional encoding logic

--- tiny_transformer.py
+++ tiny_transformer.py
@@ -44,4 +44,23 @@ def get_batch(split):
     return x.to(device), y.to(device)
 
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=1000):
+        super().__init__()
+        pe = torch.zeros(max_len, d_model)
+        pos = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+        # Handle for both even and odd d_model
+        pe[:, 0::2] = torch.sin(pos * div)
+        if d_model % 2 == 0:
+            pe[:, 1::2] = torch.cos(pos * div)
+        else:
+            pe[:, 1::2] = torch.cos(pos * div[:pe[:, 1::2].size(1)])
+
+        self.register_buffer('pe', pe)
+
+        
+
+
 
